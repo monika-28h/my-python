ae192d057228d2bfc3e79833da19c13bd828e7b2
monika-28h <monikamohan488@gmail.com>
2025-11-12 21:16:27 +0530

    Create Function to find area of a circle.py

---
+++ Function to find area of a circle.py
@@ -0,0 +1,9 @@
+# Function to find the area of a circle
+def area_of_circle(radius):
+    pi = 3.14159
+    return pi * radius ** 2
+
+# Example usage
+r = 7
+result = area_of_circle(r)
+print("The area of the circle is:", result)

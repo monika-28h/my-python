4168682c1908cb5d96b9dbcf5bcef7086a661518
monika-28h <monikamohan488@gmail.com>
2025-11-14 14:36:15 +0530

    Create  Write a Python program to find the sum and average of all elements in a list.py

---
+++ Write a Python program to find the sum and average of all elements in a list.py
@@ -0,0 +1,9 @@
+# Write a Python program to find the sum and average of all elements in a list.
+
+numbers = [10, 20, 30, 40, 50]
+
+total = sum(numbers)
+average = total / len(numbers)
+
+print("Sum:", total)
+print("Average:", average)
